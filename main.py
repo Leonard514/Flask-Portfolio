d146cf9036d94ffe98b4545a8575be28ce7c4afd
Leonard514 <leonardalwright@gmail.com>
2022-09-26 13:49:39 -0700

    Added plan webpage, integrated into navigation bar

--- main.py
+++ main.py
@@ -21,6 +21,10 @@ def index():
 def stub():
     return render_template("stub.html")
 
+@app.route('/group-plan/')  # connects /stub/ URL to stub() function
+def plan():
+    return render_template("plan.html")
+
 # this runs the application on the development server
 if __name__ == "__main__":
     app.run(debug=True)
