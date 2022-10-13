7d611363b1eb6f1b9363d810e5d52f53acb5ee69
Leonard514 <leonardalwright@gmail.com>
2022-10-13 13:34:08 -0700

    Added dictionary.py

---
+++ project_files/dictionary.py
@@ -0,0 +1,32 @@
+# List of general questions
+generalQuestionList = [{"moneyvalue": "100", "question": "Zoophobia is the fear of ______", "answer": "Animals"},
+{"moneyvalue": "200", "question": "Pierre Augustine Caron de Baumarchais play what Opera based on it", "answer": "Barber Of Seville"},
+{"moneyvalue": "300", "question": "Hang on Sloopy' was the official rock song of which band", "answer": "Ohio"},
+{"moneyvalue": "400", "question": "When is turkey traditionally eaten in america", "answer": "Thanksgiving"},
+{"moneyvalue": "500", "question": "What was mozart's first name", "answer": "Wolfgang"},
+{"moneyvalue": "600", "question": "In the film 'titanic', who did leonardo dicaprio and kate winslet play", "answer": "Jack"},
+{"moneyvalue": "700", "question": "What was founded in the UK 1694 because of war with France", "answer": "Bank Of England"}]
+
+
+# List of math questions
+mathQuestionList = [{"moneyvalue": "100", "question": "This Greek mathmetician supposedly designed a system of mirrors to set enemy ships on fire.", "answer": "Archimedes"},
+{"moneyvalue": "200", "question": "The sum of the first three digits of this irrational number is equal to the third power of one less than the first digit.", "answer": "pi"},
+{"moneyvalue": "300", "question": "This German mathematician famously feuded with Isaac Newton over which of them created calculus", "answer": "Gottfried Wilhem Leibniz"},
+{"moneyvalue": "400", "question": "It isn’t pi, but this Greek letter is commonly used to denote change", "answer": "delta"},
+{"moneyvalue": "500", "question": "This type of sum is named after a mathematician who specialized in geometry", "answer": "Riemann"},
+{"moneyvalue": "600", "question": "Nobel Prize winner and game theorist John Nash famously suffered from this mental disorder throughout his life", "answer": "schizophrenia"},
+{"moneyvalue": "700", "question": "A type of ratio, this metal is known for its softness and luster", "answer": "Gold"}]
+
+
+# List of Computer Science Questions
+CompSciQuestionList = [{"moneyvalue": "100", "question": "This Greek mathmetician supposedly designed a system of mirrors to set enemy ships on fire.", "answer": "Archimedes"},
+{"moneyvalue": "200", "question": "The sum of the first three digits of this irrational number is equal to the third power of one less than the first digit.", "answer": "pi"},
+{"moneyvalue": "300", "question": "This German mathematician famously feuded with Isaac Newton over which of them created calculus", "answer": "Gottfried Wilhem Leibniz"},
+{"moneyvalue": "400", "question": "It isn’t pi, but this Greek letter is commonly used to denote change", "answer": "delta"},
+{"moneyvalue": "500", "question": "This type of sum is named after a mathematician who specialized in geometry", "answer": "Riemann"},
+{"moneyvalue": "600", "question": "Nobel Prize winner and game theorist John Nash famously suffered from this mental disorder throughout his life", "answer": "schizophrenia"},
+{"moneyvalue": "700", "question": "A type of ratio, this metal is known for its softness and luster", "answer": "Gold"}]
+
+
+print(generalQuestionList)
+print(mathQuestionList)
