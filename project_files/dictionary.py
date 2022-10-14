3c9b6fca03ea76892700a5edad088244101ed771
Abdullah Abdul-Aziz <68267045+ABA-Aziz@users.noreply.github.com>
2022-10-14 13:50:41 -0700

    Update dictionary.py

--- project_files/dictionary.py
+++ project_files/dictionary.py
@@ -31,5 +31,25 @@ CompSciQuestionList = [{"moneyvalue": "100", "question": "This Greek mathmeticia
 {"moneyvalue": "700", "question": "A type of ratio, this metal is known for its softness and luster", "answer": "Gold"}]
 
 
+# List of Ireland Questions
+IrelandQuestionList = [{"moneyvalue": "100", "question": "This staple crop of Ireland was famously over exported to Great Britain in the early 1900s, leading to a famine.", "answer": "Potatoes"}
+{"moneyvalue": "200", "question": "Saint Patrick is known for driving all of these from Ireland, a metaphor for the Celts that kidnapped him as a child.", "answer": "Snakes"}
+{"moneyvalue": "300", "question": "Lucky! This alternate name for clovers is a staple in modern imaginations of the leprechaun.", "answer": "Shamrock"}
+{"moneyvalue": "400", "question": "Cathal Brugha was the first of these for Ireland and held this position for three years until his death in 1922.", "answer": "Prime Minister"}
+{"moneyvalue": "500", "question": "Bells of Ireland are known for symbolizing this quality, a much desired trait for the hapless.", "answer": "Good Luck"}
+{"moneyvalue": "600", "question": "#00FF00, #FFFFFF and #FFA500 is how you make the Irish flag in this color system.", "answer": "Hexadecimal"}
+{"moneyvalue": "700", "question": "Britain gained rule over Ireland in the Anglo-Saxon invasion in this year where Henry II and Louis VII made an alliance through the betrothal of their children", "answer": "1169"}
+                       
+                       
+ # List of Video Game Questions
+VideoGameQuestionList = [{"moneyvalue": "100", "question": "The company Mojang, known for creating Minecraft, is based in this country, where one of its cities is the namesake for a condition where a captive identifies with their kidnapper.", "answer": "Sweden"}
+[{"moneyvalue": "200", "question": "The creator of the Super Mario and Legend of Zelda franchises.", "answer": "Shigeru Miyamoto"}
+[{"moneyvalue": "300", "question": "A staple character in pop culture since his 1938 conception, he starred in a famously bad game on the Nintendo 64 in 1999.", "answer": "Superman"}
+[{"moneyvalue": "400", "question": "The notoriously difficult final boss of this game, along with its soundtrack, has skyrocketed this game’s popularity. Name the 2015 indie game where a child falls into the land of monsters.", "answer": "Undertale"}
+[{"moneyvalue": "500", "question": "This 1994 fully CGI show was infamously given a continuation in 2018 centered around video games.", "answer": "ReBoot"}
+[{"moneyvalue": "600", "question": "This actor had a prominent role in the marketing for the known disappointment Cyberpunk 2077. They are better known for their role in Bill and Ted’s Excellent Adventure.", "answer": "Keanu Reeves"}
+[{"moneyvalue": "700", "question": "The long running Kamen Rider franchise aired a season based on video games this year, more widely known for a large amounts of celebrities, including Prince and Alan Rickman, dying.", "answer": "2016"}
+                       
+                       
 print(generalQuestionList)
 print(mathQuestionList)
