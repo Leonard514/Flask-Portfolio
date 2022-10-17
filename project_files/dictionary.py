819f689bc6129ebd67d570e0b91e005ee6d36526
Abdullah Abdul-Aziz <68267045+ABA-Aziz@users.noreply.github.com>
2022-10-17 13:27:51 -0700

    Update dictionary.py

--- project_files/dictionary.py
+++ project_files/dictionary.py
@@ -3,16 +3,16 @@
 
 # List of general questions
 generalQuestionList = [{"moneyvalue": "100", "question": "Zoophobia is the fear of ______", "answer": "Animals"},
-{"moneyvalue": "200", "question": "Pierre Augustine Caron de Baumarchais play what Opera based on it", "answer": "Barber Of Seville"},
+{"moneyvalue": "200", "question": "Pierre Augustine Caron de Baumarchais plays what Opera based on it", "answer": "Barber Of Seville"},
 {"moneyvalue": "300", "question": "Hang on Sloopy' was the official rock song of which band", "answer": "Ohio"},
-{"moneyvalue": "400", "question": "When is turkey traditionally eaten in america", "answer": "Thanksgiving"},
-{"moneyvalue": "500", "question": "What was mozart's first name", "answer": "Wolfgang"},
-{"moneyvalue": "600", "question": "In the film 'titanic', who did leonardo dicaprio and kate winslet play", "answer": "Jack"},
+{"moneyvalue": "400", "question": "When is Turkey traditionally eaten in America", "answer": "Thanksgiving"},
+{"moneyvalue": "500", "question": "What was Mozart's first name", "answer": "Wolfgang"},
+{"moneyvalue": "600", "question": "In the film 'Titanic', who did Leonardo DiCaprio play", "answer": "Jack"},
 {"moneyvalue": "700", "question": "What was founded in the UK 1694 because of war with France", "answer": "Bank Of England"}]
 
 
-# List of math questions
-mathQuestionList = [{"moneyvalue": "100", "question": "This Greek mathmetician supposedly designed a system of mirrors to set enemy ships on fire.", "answer": "Archimedes"},
+# List of Math questions
+MathQuestionList = [{"moneyvalue": "100", "question": "This Greek mathmetician supposedly designed a system of mirrors to set enemy ships on fire.", "answer": "Archimedes"},
 {"moneyvalue": "200", "question": "The sum of the first three digits of this irrational number is equal to the third power of one less than the first digit.", "answer": "pi"},
 {"moneyvalue": "300", "question": "This German mathematician famously feuded with Isaac Newton over which of them created calculus", "answer": "Gottfried Wilhem Leibniz"},
 {"moneyvalue": "400", "question": "It isn’t pi, but this Greek letter is commonly used to denote change", "answer": "delta"},
@@ -22,13 +22,13 @@ mathQuestionList = [{"moneyvalue": "100", "question": "This Greek mathmetician s
 
 
 # List of Computer Science Questions
-CompSciQuestionList = [{"moneyvalue": "100", "question": "This Greek mathmetician supposedly designed a system of mirrors to set enemy ships on fire.", "answer": "Archimedes"},
-{"moneyvalue": "200", "question": "The sum of the first three digits of this irrational number is equal to the third power of one less than the first digit.", "answer": "pi"},
-{"moneyvalue": "300", "question": "This German mathematician famously feuded with Isaac Newton over which of them created calculus", "answer": "Gottfried Wilhem Leibniz"},
-{"moneyvalue": "400", "question": "It isn’t pi, but this Greek letter is commonly used to denote change", "answer": "delta"},
-{"moneyvalue": "500", "question": "This type of sum is named after a mathematician who specialized in geometry", "answer": "Riemann"},
-{"moneyvalue": "600", "question": "Nobel Prize winner and game theorist John Nash famously suffered from this mental disorder throughout his life", "answer": "schizophrenia"},
-{"moneyvalue": "700", "question": "A type of ratio, this metal is known for its softness and luster", "answer": "Gold"}]
+CompSciQuestionList = [{"moneyvalue": "100", "question": "JavaScript requires all commands to end with this character", "answer": ";"},
+{"moneyvalue": "200", "question": "The creator of Python, Guido van Rossum, is Dutch. The capital of his home country is this city.", "answer": "Amsterdam"},
+{"moneyvalue": "300", "question": "When a phrase is encased in quotation marks (""), it is known as this.", "answer": "string"},
+{"moneyvalue": "400", "question": "What is the system used to fetch web pages based on a URL (ex: google.com)?", "answer": "Domain Name System (DNS)"},
+{"moneyvalue": "500", "question": "How would you write an HTML comment with the content “Stores variables” (without quotes)?", "answer": "<!-- Stores variables -->"},
+{"moneyvalue": "600", "question": "Sharing a name with a fairy-like creature in European folklore, a collection of images put into a single image in JavaScript.", "answer": "Sprite"},
+{"moneyvalue": "700", "question": "C++ was created by this Danish computer scientist in 1983.", "answer": "Bjarne Stroustrup"}]
 
 
 # List of Ireland Questions
@@ -50,6 +50,35 @@ VideoGameQuestionList = [{"moneyvalue": "100", "question": "The company Mojang, 
 [{"moneyvalue": "600", "question": "This actor had a prominent role in the marketing for the known disappointment Cyberpunk 2077. They are better known for their role in Bill and Ted’s Excellent Adventure.", "answer": "Keanu Reeves"}
 [{"moneyvalue": "700", "question": "The long running Kamen Rider franchise aired a season based on video games this year, more widely known for a large amounts of celebrities, including Prince and Alan Rickman, dying.", "answer": "2016"}
                        
+ 
+ # List of General Science Questions
+ GenSciQuestionList = [{"moneyvalue": "100", "question": "Alan Turing, the inventor of the computer, was a World War II code breaker for this country.", "answer": "England"}
+[{"moneyvalue": "200", "question": "Edward Jenner developed the smallpox vaccine after noticing the maids who had contracted this disease never contracted smallpox", "answer": "Cowpox"}
+[{"moneyvalue": "300", "question": "Alfred Nobel, founder of the Nobel Prize, created this material used in construction and demolition.", "answer": "Dynamite"}
+[{"moneyvalue": "400", "question": "Neodymium, a metal used in super magnets, is an element in this group on the periodic table.", "answer": "Lanthanides"}
+[{"moneyvalue": "500", "question": "Marble is a metamorphic rock that came from this sedimentary rock.", "answer": "Limestone"}
+[{"moneyvalue": "600", "question": "Cortisol is a hormone released by the brain when the body is undergoing this emotion.", "answer": "Stress"}
+[{"moneyvalue": "700", "question": "This animal’s scientific name is the same word twice.", "answer": "Skunk"}
+ 
+
+ # List of Literature Questions
+ GenLitQuestionList = [{"moneyvalue": "100", "question": "The author of War of the Worlds and the Time Machine.", "answer": "H. G. Wells"}
+[{"moneyvalue": "200", "question": "The protagonists in the John Green novel The Fault in Our Stars tastelessly kiss where this Dutch girl hid with her family during the Holocaust.", "answer": "Anne Frank"}
+[{"moneyvalue": "300", "question": "Ponyboy Curtis recites a poem by this man, who also wrote The Road Not Taken.", "answer": "Robert Frost"
+[{"moneyvalue": "400", "question": "Mary Shelley’s 1816 Romantic novel The Modern Prometheus is said to have kickstarted this genre.", "answer": "Science Fiction"
+[{"moneyvalue": "500", "question": "The protagonist of this novel exchanges emails with a fellow gay student at his school, and risks getting outed by one of his peers. It recieved a film adaptation in 2018.", "answer": "Simon vs. the Homo Sapiens Agenda"
+[{"moneyvalue": "600", "question": "Khalil Gibran details his thoughts on society in his 1925 book named for this holy figure.", "answer": "Prophet"}
+[{"moneyvalue": "700", "question": "A chapter in this 1008 novel is completely blank to signify the death of the viewpoint character.", "answer": "The Tale of Genji"}
+  
+  
+# List of Sports Questions
+  SportsQuestionList = [{"moneyvalue": "100", "question": "In Soccer / Fútbol, which player has the most Ballen D’ors?", "answer": "Lionel Messi"}
+[{"moneyvalue": "200", "question": "Who has the most touchdowns in NFL history?", "answer": "Jerry Rice"}
+[{"moneyvalue": "300", "question": "Who won the 2018 Russia World Cup (Fútbol)?", "answer": "France"}
+[{"moneyvalue": "400", "question": "Which two teams were in the 2021 NBA Finals? Name one or both.", "answer": "Milwaukee Bucks & Phoenix Suns"}
+[{"moneyvalue": "500", "question": "Out of his 61 professional fights, how many fights did Muhhamad Ali win?", "answer": "56"}
+[{"moneyvalue": "600", "question": "Tom Hanks says 'There’s no crying in baseball!' in this 1992 film.", "answer": "A Leauge of Their Own"}
+[{"moneyvalue": "700", "question": "The national sport of Pakistan, while not its most famous sport, was portrayed in the 2007 Bollywood film Chak de! India", "answer": "Field Hockey"}
                        
 print(generalQuestionList)
 print(mathQuestionList)
