4e83f7a967b6709af1dba9f0e90b4e34c3006d30
Martynguyen64 <martinsupercell06@gmail.com>
2022-11-03 18:24:41 -0700

    dictionary

--- project_files/dictionary.py
+++ project_files/dictionary.py
@@ -4,84 +4,34 @@
 # answer is self-explanatory
 
 
-# List of General Questions
-GeneralQuestionList = [{"moneyvalue": "100", "question": "Zoophobia is the fear of ______", "answer": "Animals"},
-{"moneyvalue": "200", "question": "Pierre Augustine Caron de Baumarchais plays what Opera based on it", "answer": "Barber Of Seville"},
-{"moneyvalue": "300", "question": "Hang on Sloopy' was the official rock song of which band", "answer": "Ohio"},
-{"moneyvalue": "400", "question": "When is turkey traditionally eaten in America", "answer": "Thanksgiving"},
-{"moneyvalue": "500", "question": "What was Mozart's first name", "answer": "Wolfgang"},
-{"moneyvalue": "600", "question": "In the film 'Titanic', who did Leonardo DiCaprio play", "answer": "Jack"},
-{"moneyvalue": "700", "question": "What was founded in the UK 1694 because of war with France", "answer": "Bank Of England"}]
-
-
-# List of Math Questions
-MathQuestionList = [{"moneyvalue": "100", "question": "This Greek mathmetician supposedly designed a system of mirrors to set enemy ships on fire.", "answer": "Archimedes"},
-{"moneyvalue": "200", "question": "The sum of the first three digits of this irrational number is equal to the third power of one less than the first digit.", "answer": "pi"},
-{"moneyvalue": "300", "question": "If an equation has exactly one y-value for every x-value, it is a ...", "answer": "function"},
-{"moneyvalue": "400", "question": "It isn’t pi, but this Greek letter is commonly used to denote change", "answer": "delta"},
-{"moneyvalue": "500", "question": "This type of sum is named after a mathematician who specialized in geometry", "answer": "Riemann"},
-{"moneyvalue": "600", "question": "This German mathematician famously feuded with Isaac Newton over which of them created calculus", "answer": "Gottfried Leibniz"},
-{"moneyvalue": "700", "question": "A type of ratio, this metal is known for its softness and luster", "answer": "Gold"}]
-
-
-# List of Computer Science Questions
-CompSciQuestionList = [{"moneyvalue": "100", "question": "JavaScript requires all commands to end with this character", "answer": ";"},
-{"moneyvalue": "200", "question": "The creator of Python, Guido van Rossum, is Dutch. The capital of his home country is this city.", "answer": "Amsterdam"},
-{"moneyvalue": "300", "question": "When a phrase is encased in quotation marks (""), it is known as this.", "answer": "string"},
-{"moneyvalue": "400", "question": "What is the system used to fetch web pages based on a URL (ex: google.com)?", "answer": "Domain Name System (DNS)"},
-{"moneyvalue": "500", "question": "How would you write an HTML comment with the content “Stores variables” (without quotes)?", "answer": "<!-- Stores variables -->"},
-{"moneyvalue": "600", "question": "Sharing a name with a fairy-like creature in European folklore, a collection of images put into a single image in JavaScript.", "answer": "Sprite"},
-{"moneyvalue": "700", "question": "C++ was created by this Danish computer scientist in 1983.", "answer": "Bjarne Stroustrup"}]
-
-
-# List of Ireland Questions
-IrelandQuestionList = [{"moneyvalue": "100", "question": "This staple crop of Ireland was famously over exported to Great Britain in the early 1900s, leading to a famine.", "answer": "Potatoes"},
-{"moneyvalue": "200", "question": "Saint Patrick is known for driving all of these from Ireland, a metaphor for the Celts that kidnapped him as a child.", "answer": "Snakes"},
-{"moneyvalue": "300", "question": "Lucky! This alternate name for clovers is a staple in modern imaginations of the leprechaun.", "answer": "Shamrock"},
-{"moneyvalue": "400", "question": "Cathal Brugha was the first of these for Ireland and held this position for three years until his death in 1922.", "answer": "Prime Minister"},
-{"moneyvalue": "500", "question": "Bells of Ireland are known for symbolizing this quality, a much desired trait for the hapless.", "answer": "Good Luck"},
-{"moneyvalue": "600", "question": "#00FF00, #FFFFFF and #FFA500 is how you make the Irish flag in this color system.", "answer": "Hexadecimal"},
-{"moneyvalue": "700", "question": "Britain gained rule over Ireland in the Anglo-Saxon invasion in this year where Henry II and Louis VII made an alliance through the betrothal of their children", "answer": "1169"}]
-                       
-                       
- # List of Video Game Questions
-VideoGameQuestionList = [{"moneyvalue": "100", "question": "The company Mojang, known for creating Minecraft, is based in this country, where one of its cities is the namesake for a condition where a captive identifies with their kidnapper.", "answer": "Sweden"},
-{"moneyvalue": "200", "question": "The creator of the Super Mario and Legend of Zelda franchises.", "answer": "Shigeru Miyamoto"},
-{"moneyvalue": "300", "question": "A staple character in pop culture since his 1938 conception, he starred in a famously bad game on the Nintendo 64 in 1999.", "answer": "Superman"},
-{"moneyvalue": "400", "question": "The notoriously difficult final boss of this game, along with its soundtrack, has skyrocketed this games popularity. Name the 2015 indie game where a child falls into the land of monsters.", "answer": "Undertale"},
-{"moneyvalue": "500", "question": "This 1994 fully CGI show was infamously given a continuation in 2018 centered around video games.", "answer": "ReBoot"},
-{"moneyvalue": "600", "question": "This actor had a prominent role in the marketing for the known disappointment Cyberpunk 2077. They are better known for their role in Bill and Ted’s Excellent Adventure.", "answer": "Keanu Reeves"},
-{"moneyvalue": "700", "question": "The long running Kamen Rider franchise aired a season based on video games this year, more widely known for a large amounts of celebrities, including Prince and Alan Rickman, dying.", "answer": "2016"}]
-                       
- 
- # List of General Science Questions
-GenSciQuestionList = [{"moneyvalue": "100", "question": "Alan Turing, the inventor of the computer, was a World War II code breaker for this country.", "answer": "England"},
-{"moneyvalue": "200", "question": "Edward Jenner developed the smallpox vaccine after noticing the maids who had contracted this disease never contracted smallpox", "answer": "Cowpox"},
-{"moneyvalue": "300", "question": "Alfred Nobel, founder of the Nobel Prize, created this material used in construction and demolition.", "answer": "Dynamite"},
-{"moneyvalue": "400", "question": "Neodymium, a metal used in super magnets, is an element in this group on the periodic table.", "answer": "Lanthanides"},
-{"moneyvalue": "500", "question": "Marble is a metamorphic rock that came from this sedimentary rock.", "answer": "Limestone"},
-{"moneyvalue": "600", "question": "Cortisol is a hormone released by the brain when the body is undergoing this emotion.", "answer": "Stress"},
-{"moneyvalue": "700", "question": "This animals scientific name is the same word twice.", "answer": "Skunk"}]
- 
-
- # List of Literature Questions
-GenLitQuestionList = [{"moneyvalue": "100", "question": "The author of War of the Worlds and the Time Machine.", "answer": "H. G. Wells"},
-{"moneyvalue": "200", "question": "The protagonists in the John Green novel The Fault in Our Stars tastelessly kiss where this Dutch girl hid with her family during the Holocaust.", "answer": "Anne Frank"},
-{"moneyvalue": "300", "question": "Ponyboy Curtis recites a poem by this man, who also wrote The Road Not Taken.", "answer": "Robert Frost"},
-{"moneyvalue": "400", "question": "Mary Shelley’s 1816 Romantic novel The Modern Prometheus is said to have kickstarted this genre.", "answer": "Science Fiction"},
-{"moneyvalue": "500", "question": "The protagonist of this novel exchanges emails with a fellow gay student at his school, and risks getting outed by one of his peers. It recieved a film adaptation in 2018.", "answer": "Simon vs. the Homo Sapiens Agenda"},
-{"moneyvalue": "600", "question": "Khalil Gibran details his thoughts on society in his 1925 book named for this holy position.", "answer": "Prophet"},
-{"moneyvalue": "700", "question": "A chapter in this 1008 novel is completely blank to signify the death of the viewpoint character.", "answer": "The Tale of Genji"}]
-  
-  
-# List of Sports Questions
-SportsQuestionList = [{"moneyvalue": "100", "question": "In Soccer / Fútbol, which player has the most Ballen D’ors?", "answer": "Lionel Messi"},
-{"moneyvalue": "200", "question": "Who has the most three pointers in NBA history?", "answer": "Steph Curry"},
-{"moneyvalue": "300", "question": "Who won the 2018 Russia World Cup (Fútbol)?", "answer": "France"},
-{"moneyvalue": "400", "question": "Who has the most touchdowns in NFL history?", "answer": "Jerry Rice"},
-{"moneyvalue": "500", "question": "How much is a field goal worth on its own? (no touchdowns scored before)", "answer": "3"},
-{"moneyvalue": "600", "question": "Tom Hanks says 'There’s no crying in baseball!' in this 1992 film.", "answer": "A Leauge of Their Own"},
-{"moneyvalue": "700", "question": "The national sport of Pakistan, while not its most famous sport, was portrayed in the 2007 Bollywood film Chak de! India", "answer": "Field Hockey"}]
-                       
-print(GeneralQuestionList)
-print(MathQuestionList)
+NBAteams = [{"Atlanta Hawks": "https://www.basketball-reference.com/teams/ATL/"},
+{"Boston Celtics": "https://www.basketball-reference.com/teams/BOS/"},
+{"Brooklyn Nets": "https://www.basketball-reference.com/teams/NJN/"},
+{"Charlotte Hornets": "https://www.basketball-reference.com/teams/CHA/"},
+{"Chicago Bulls": "https://www.basketball-reference.com/teams/CHI/"},
+{"Cleveland Cavaliers": "https://www.basketball-reference.com/teams/CLE/"},
+{"Dallas Mavericks": "https://www.basketball-reference.com/teams/DAL/"},
+{"Denver Nuggets": "https://www.basketball-reference.com/teams/DEN/"},
+{"Detroit Pistons": "https://www.basketball-reference.com/teams/DET/"},
+{"Golden State Warriors": "https://www.basketball-reference.com/teams/GSW/"},
+{"Houston Rockets": "https://www.basketball-reference.com/teams/HOU/"},
+{"Indiana Pacers": "https://www.basketball-reference.com/teams/IND/"},
+{"Los Angeles Clippers": "https://www.basketball-reference.com/teams/LAC/"},
+{"Los Angeles Lakers": "https://www.basketball-reference.com/teams/LAL/"},
+{"Memphis Grizzlies": "https://www.basketball-reference.com/teams/MEM/"},
+{"Miami Heat": "https://www.basketball-reference.com/teams/MIA/"},
+{"Milwaukee Bucks": "https://www.basketball-reference.com/teams/MIL/"},
+{"Minnesota Timberwolves": "https://www.basketball-reference.com/teams/MIN/"},
+{"New Orleans Pelicans": "https://www.basketball-reference.com/teams/NOH/"},
+{"New York Knicks": "https://www.basketball-reference.com/teams/NYK/"},
+{"Oklahoma City Thunder": "https://www.basketball-reference.com/teams/OKC/"},
+{"Orlando Magic": "https://www.basketball-reference.com/teams/ORL/"},
+{"Philadelphia 76ers": "https://www.basketball-reference.com/teams/PHI/"},
+{"Phoenix Suns": "https://www.basketball-reference.com/teams/PHO/"},
+{"Portland Trail Blazers": "https://www.basketball-reference.com/teams/POR/"},
+{"Sacramento Kings": "https://www.basketball-reference.com/teams/SAC/"},
+{"San Antonio Spurs": "https://www.basketball-reference.com/teams/SAS/"},
+{"Toronto Raptors": "https://www.basketball-reference.com/teams/TOR/"},
+{"Utah Jazz": "https://www.basketball-reference.com/teams/UTA/"},
+{"Washington Wizards": "https://www.basketball-reference.com/teams/WAS/"},
+]
